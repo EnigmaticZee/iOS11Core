a9c79223b63e48fa370e9e60876f7940f79dcde6
EnigmaticZee <zaidshakilz@gmail.com>
2018-02-25 03:50:16 +1100

    IAmRich App Update

---
+++ AmIRich/AmIRich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AmIRich
+//
+//  Created by Zaid on 2018-02-25.
+//  Copyright © 2018 Zaid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
