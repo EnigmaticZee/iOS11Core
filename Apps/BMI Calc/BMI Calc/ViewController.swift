5a4861e88b51f9f74bd9634897c7607eeacb2af5
EnigmaticZee <zaidshakilz@gmail.com>
2018-02-26 22:56:08 +1100

    BMI Calculator v1

---
+++ Apps/BMI Calc/BMI Calc/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  BMI Calc
+//
+//  Created by Zaid on 2018-02-26.
+//  Copyright © 2018 Zaid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var weightKg: UITextField!
+    @IBOutlet weak var heightM: UITextField!
+    @IBOutlet weak var calcBtn: UIButton!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func calculateBMI() -> String {
+        let numone = NSString(string: weightKg.text!).doubleValue
+        let num2 = NSString(string: heightM.text!).doubleValue
+        var message = ""
+        let calC = numone/pow(num2, 2)
+        if (calC > 25){
+            message = "Oi!!! mate, you are overweight!"
+        }
+        else if (calC > 18.5 && calC < 25){
+            message = "Awesome! your weight is normal"
+        }
+        else {
+            message = "You are underweight"
+        }
+        let formatted = String(format: "$.2f", calC)
+        return "Your BMI is \(formatted) and \(message)"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func calcBtnPressed(_ sender: UIButton)
+    {
+        resultLabel.text = calculateBMI()
+    }
+    
+}
+
--- Playground/99 Bottles of Bear.playground/Contents.swift
+++ Playground/99 Bottles of Bear.playground/Contents.swift
@@ -1,14 +1,32 @@
 import UIKit
 
-func bearSong(bottles: Int) -> String {
-    var lyrics: String = ""
-    for number in (1...bottles).reversed() {
-        let newLine: String = "\n\(number) bottles of the bear on the wall, \(number) bottles of beer. \nTake one down and pass it around, \(number-1) bottles of bear on the wall\n"
-        lyrics += newLine
-    }
-    
-    lyrics += "\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall."
-    return lyrics
+func bearBottle(number: Int){
+        for number in (1...number).reversed(){
+            if number > 1 {
+                print("\n\(number) bottles of the bear on the wall, \(number) bottles of beer. \nTake one down and pass it around, \(number-1) bottles of bear on the wall\n")
+            }
+            else if number == 1
+            {
+                print("\n\(number) bottles of the bear on the wall, \(number) bottles of beer. \nTake one down and pass it around, no bottles of bear on the wall\n")
+            }
+        }
 }
+bearBottle(number: 99)
 
-print(bearSong(bottles: 10))
+
+//String = "\n\(number) bottles of the bear on the wall, \(number) bottles of beer. \nTake one down and pass it around, \(number-1) bottles of bear on the wall\n"
+
+
+//func bearSong(bottles: Int) -> String {
+//    var lyrics: String = ""
+//    for number in (1...bottles).reversed() {
+//        let newLine: String = "\n\(number) bottles of the bear on the wall, \(number) bottles of beer. \nTake one down and pass it around, \(number-1) bottles of bear on the wall\n"
+//        lyrics += newLine
+//    }
+//
+//    lyrics += "\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall."
+//    return lyrics
+//}
+//
+//print(bearSong(bottles: 10))
+
