6790fa698f33773d4f2d4c69c2f82f34b4c76aa9
EnigmaticZee <zaidshakilz@gmail.com>
2018-02-28 23:51:07 +1100

    Update

--- Playground/Fibonacci.playground/Contents.swift
+++ Playground/Fibonacci.playground/Contents.swift
@@ -1,23 +1,30 @@
 import UIKit
 
-//func fibonacci(until n : Int){
-//    print(0)
-//    print(1)
-//    var num = 0
-//    var num2 = 1
-//    for k in 0...n {
-//        let num = num + num2
-//    }
-//}
-
-func euler1() {
-    var sum = 0
-    for i in 0...999 {
-        if i%3 == 0 || i%5 == 0 {
-            sum += i
-        }
+func fibo(n : Int) {
+    var n1 = 0
+    var n2 = 1
+    print(0)
+    print(1)
+    for i in 0...n {
+        let sum = n1 + n2
+        print(sum)
+        n1 = n2
+        n2 = sum
     }
-    print(sum)
 }
 
-euler1()
+fibo(n: 4)
+
+
+//func euler1() {
+//    var sum = 0
+//    for i in 0...999 {
+//        if i%3 == 0 || i%5 == 0 {
+//            sum += i
+//        }
+//    }
+//    print(sum)
+//}
+//
+//euler1()
+
